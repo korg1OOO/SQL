f402645f680386a51c0dd75928984d60a1326646
Enzo Michel Barbosa Feldman <enzomichelfeldman@gmail.com>
2024-10-07 14:33:59 -0300

    Update hospital.sql

--- hospital.sql
+++ hospital.sql
@@ -1,1 +1,29 @@
+create database hospital;
+show databases;
+use hospital;
 
+create table medico(
+codMed int primary key,
+nomeMed varchar(40) not null,
+dataNasc date not null,
+cpf char(11),
+cidade varchar(30) not null,
+especialidade varchar(30) not null
+);
+
+create table paciente(
+codPac int primary key,
+nomePac varchar(40) not null,
+dataNasc date not null,
+cpf char(11), cidade varchar(30) not null
+);
+
+create table consulta(
+codMed int not null,
+codPac int not null,
+dataConsulta date not null,
+hora time not null,
+primary key(codMed, codPac),
+foreign key(codMed) references medico(codMed),
+foreign key(codPac) references paciente(codPac)
+);
