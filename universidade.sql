a0c61f77bb8760822563ff95b45bdfa5dfe00e3c
Enzo Michel Barbosa Feldman <enzomichelfeldman@gmail.com>
2024-10-07 14:49:55 -0300

    Create universidade.sql

---
+++ universidade.sql
@@ -0,0 +1,26 @@
+create database universidade;
+show databases;
+use universidade;
+
+create table professor(
+codProf int primary key,
+nomeProf varchar(40) not null,
+dataNasc date not null,
+cpf char(11),
+area varchar(30) not null
+);
+
+create table disciplina(
+codDisc int primary key,
+nomeDisc varchar(40) not null,
+cargaHora varchar(10) not null
+);
+
+show databases;
+create table ministra(
+codProf int not null,
+codDisc int not null,
+primary key(codProf, codDisc),
+foreign key(codProf) references professor(codProf),
+foreign key(codDisc) references disciplina(codDisc)
+);
